56535d3b55cf8a5b9d93f522be10192e565c92c7
Alexander Cochrane <acochrane@almightycorp.com>
2017-04-10 19:44:09 -0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/data_structures_hashing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_structures_hashing.dir/main.cpp.o"
+  "data_structures_hashing.pdb"
+  "data_structures_hashing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/data_structures_hashing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
